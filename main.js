877cba50f03bd3f03d9fa25eabf2071dacffe015
Piyush Chatterjee <117717525+Omegahelkarex@users.noreply.github.com>
2023-11-20 16:22:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+// navbar style scroll 
+
+window.addEventListener('scroll', () =>{
+    document.querySelector('nav').classList.toggle
+    ('window-scroll', window.scrollY > 0 )
+})
+
+
+
+// faq 
+
+const faqs = document.querySelectorAll('.faq');
+
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () =>{
+        faq.classList.toggle('open');
+
+        // icon change 
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === "fa-solid fa-plus") {
+            icon.className = "fa-solid fa-minus"
+        }
+        else{
+            icon.className = "fa-solid fa-plus"
+        }
+
+    })
+})
+
+
+//dropbar navbar
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click', closeNav)
