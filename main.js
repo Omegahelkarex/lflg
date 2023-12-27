4a76d1e0126908f046d8abb65256a2701dd27364
Piyush Chatterjee <117717525+Omegahelkarex@users.noreply.github.com>
2023-12-27 18:47:15 +0530

    final commit

--- main.js
+++ main.js
@@ -1,21 +1,4 @@
-// dark mode 
 
-const dayNight = document.querySelector(".day-night");
-dayNight.addEventListener("click", () => {
-    dayNight.querySelector("i").classList.toggle("fa-sun");
-    dayNight.querySelector("i").classList.toggle("fa-moon");
-    document.body.classList.toggle("light");
-
-    
-})
-window.addEventListener("load", () => {
-    if(document.body.classList.contains("light")){
-        dayNight.querySelector("i").classList.add("fa-sun");
-    }
-    else {
-        dayNight.querySelector("i").classList.add("fa-moon");
-    }
-})
 
 // navbar style scroll 
 
@@ -68,4 +51,26 @@ const closeNav = () => {
     menuBtn.style.display = "inline-block";
 }
 
-closeBtn.addEventListener('click', closeNav)
+closeBtn.addEventListener('click', closeNav);
+
+// dark mode 
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("light");
+
+    
+})
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("light")){
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
+
+
+
