c94b320de3e9460d50b5f9eacc63af624f26a608
Piyush Chatterjee <117717525+Omegahelkarex@users.noreply.github.com>
2023-12-29 14:30:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ closeBtn.addEventListener('click', closeNav);
 
 const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () => {
-    dayNight.querySelector("i").classList.toggle("fa-sun spin");
+    dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
     document.body.classList.toggle("light");
 
